68cb394e050c42fb699ba30c86105a3dbdc9d54a
ranjithnaidu24 <tndkranjithnaidu@gmail.com>
2024-09-18 20:07:30 +0530

    initial commit

---
+++ src/com/Start.java
@@ -0,0 +1,9 @@
+package com;
+
+public class Start {
+
+	public static void main(String[] args) {
+		User user=new User();
+		BankOperations bank=new BankOperations();
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module FunctionalATMinJAVA {
+}
