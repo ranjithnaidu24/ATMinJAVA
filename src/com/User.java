7fb1f1b4c4c42596e5fd6fba9bf9e3079df8b69a
ranjithnaidu24 <tndkranjithnaidu@gmail.com>
2024-09-18 20:36:37 +0530

    created user attributes in user class

--- src/com/User.java
+++ src/com/User.java
@@ -1,10 +1,72 @@
 package com;
 
-public class User {
-    private String userName;
-    private int accNo;
-    private String cardNo;
-    private int pin;
-    private double balance;
+import java.util.ArrayList;
+import java.util.List;
 
+public class User {
+	private String userName;
+	private int accNo;
+	private String cardNo;
+	private int pin;
+	private double balance;
+	private List<String> transactionHistory;
+
+	public User() {
+	}
+
+	public User(String userName, int accNo, String cardNo, int pin, double balance) {
+		this.userName = userName;
+		this.accNo = accNo;
+		this.cardNo = cardNo;
+		this.pin = pin;
+		this.balance = balance;
+		this.transactionHistory = new ArrayList<>();
+	}
+
+	public void setUsername(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setAccNo(int accNo) {
+		this.accNo = accNo;
+	}
+
+	public int getAccNo() {
+		return accNo;
+	}
+
+	public void setCardNo(String cardNo) {
+		this.cardNo = cardNo;
+	}
+
+	public String getCardNo() {
+		return cardNo;
+	}
+
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+
+	public int getPin() {
+		return pin;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void addTransaction(String transactionDetail) {
+		transactionHistory.add(transactionDetail);
+	}
+	public List<String> getTransactionHistory(){
+		return transactionHistory;
+	}
 }
